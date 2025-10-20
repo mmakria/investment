71314d161e48012194b466ad4cef9ff4b629ecdd
momomak <m.makria@gmail.com>
2025-10-21 00:48:52 +0200

    playing with functions

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,22 +5,43 @@ import (
 	"math"
 )
 
-func main() {
+const inflationRate = 2.5
 
-	const inflationRate = 2.5
+func main() {
 	var investmentAmount float64
 	var years float64
 	expectedReturnRate := 5.5
 
-	fmt.Print("Enter an investment Amount: ")
+	//fmt.Print("Enter an investment Amount: ")
+	outputText("Enter an investment Amount: ")
 	fmt.Scan(&investmentAmount)
-	fmt.Print("Enter years: ")
+	//fmt.Print("Enter years: ")
+	outputText("Enter Years: ")
 	fmt.Scan(&years)
 
-	futureValue := (investmentAmount) * math.Pow((1+expectedReturnRate/100), years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, years, expectedReturnRate)
 
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	//futureValue := (investmentAmount) * math.Pow(1+expectedReturnRate/100, years)
+	//futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Printf("Future value is %0.2f \nFuture value including inflation is %0.2f ", futureValue, futureRealValue)
+	formattedFv := fmt.Sprintf("Future value is %0.2f \n", futureValue)
+	formattedRfv := fmt.Sprintf("Future real value is %0.2f \n", futureRealValue)
+	// fmt.Printf("Future value is %0.2f \nFuture value including inflation is %0.2f ", futureValue, futureRealValue)
 
+	fmt.Print(formattedFv, formattedRfv)
+	outputText(formattedFv)
+	outputText(formattedRfv)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount,
+	expectedReturnRate,
+	years float64) (fv float64, frv float64) {
+	fv = (investmentAmount) * math.Pow(1+expectedReturnRate/100, years)
+	frv = fv / math.Pow(1+inflationRate/100, years)
+	return fv, frv
+	//return
 }
